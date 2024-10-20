6424761a1574e378d7e13870edcf5d84ade5beee
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2024-10-19 21:13:34 -0600

    mejora

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permitir solo este origen
+    credentials: true, // Habilitar envío de credenciales como cookies o cabeceras autorizadas
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -3,12 +3,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
-import { Role } from '../users/role.entity';  // Ajusta la ruta si es necesario
+import { Role } from '../users/role.entity';  
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
   providers: [RolesService],
   controllers: [RolesController],
-  exports: [RolesService, TypeOrmModule],  // Exporta el servicio si se necesita en otros módulos
+  exports: [RolesService, TypeOrmModule],  
 })
 export class RolesModule {}
